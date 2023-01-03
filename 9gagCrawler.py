afd18356f23ac7a6ddb00197cb44c7b7d98434a3
Katharina S <Kat-Schreiber@web.de>
2023-01-03 19:01:16 +0100

    add csv output

--- 9gagCrawler.py
+++ 9gagCrawler.py
@@ -2,6 +2,7 @@ import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
 
 url = "https://9gag.com/search/?query=climate%20change"
 
@@ -17,18 +18,23 @@ lastHeight = driver.execute_script("return document.body.scrollHeight")
 
 def crawlData(articles):
     #articles = soup.find_all("article")
+    metadata = []
     for article in articles:
+        article_meta = {}
+        article_meta["id"] = article["id"]
         print("")
         print(article["id"])
         try:
             creatorID = article.find("a", class_ = "ui-post-creator__author")
             print(creatorID.contents[0])
+            article_meta["creatorID"] = creatorID.contents[0]
         except:
             print("no id")
             continue
         try:
             headerText = article.find("h2")
             print(headerText.contents[0])
+            article_meta["headerText"] = headerText.contents[0]
         except:
             print("no header")
             continue
@@ -36,29 +42,41 @@ def crawlData(articles):
             picture = article.find("picture")
             img = picture.find("img")
             print(img["src"])
+            article_meta["imgSrc"] = img["src"]
+            img2 = requests.get(img["src"]).content
+            with open(f'images\{article["id"]}.jpg', 'wb') as handler: 
+                handler.write(img2)
+                pass
         except:
-            try:
-                video = article.find("div", class_ = "post-view video-post")
-                vid = video.find("video")
-                print("found video")
-            except:
-                print("no img or video found")
+            continue
+            #try:
+            #    video = article.find("div", class_ = "post-view video-post")
+            #    vid = video.find("video")
+            #    print("found video")
+            #except:
+            #    print("no img or video found")
+
+        metadata.append(article_meta)
+    return metadata
+
+allMeta = []       
 
 pause = 1.5
-for i in range(5):
+for i in range(50):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(pause)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     #print(soup)
     articles = soup.find_all("article")
-    crawlData(articles)
+    allMeta += crawlData(articles)
     newHeight = driver.execute_script("return document.body.scrollHeight")
     if newHeight == lastHeight:
         break
     lastHeight = newHeight
     #print(lastHeight)
 
-
+df = pd.DataFrame(allMeta)
+df.to_csv("imgData.csv")
 
 
 
